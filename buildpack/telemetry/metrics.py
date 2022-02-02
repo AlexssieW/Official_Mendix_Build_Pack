267ae467decf42c1f3be14df17c1bfb9efb7d166
Denis Shuvalov <denis.shuvalov@mendix.com>
2022-02-02 09:30:50 +0100

    statsd added

--- buildpack/telemetry/metrics.py
+++ buildpack/telemetry/metrics.py
@@ -55,7 +55,7 @@ PAIDAPPS_METRICS_REGISTRY = [
 STATSD_REGESTRY = {
     "type": "statsd",
     "settings": {
-      "host": 'app.datadoghq.eu',
+      "host": 'gfhfhg',
       "port": datadog.get_statsd_port()
     }
   }

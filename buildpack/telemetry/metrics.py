7d88886c90c07dc7bdd3a0f7694e7c3ce9440574
Denis Shuvalov <denis.shuvalov@mendix.com>
2022-02-02 09:48:36 +0100

    statsd added

--- buildpack/telemetry/metrics.py
+++ buildpack/telemetry/metrics.py
@@ -55,7 +55,7 @@ PAIDAPPS_METRICS_REGISTRY = [
 STATSD_REGESTRY = {
     "type": "statsd",
     "settings": {
-      "host": 'app.datadoghq.eu',
+      "host": datadog.get_api_url(),
       "port": datadog.get_statsd_port()
     }
   }
@@ -177,9 +177,9 @@ def configure_influx_registry(m2ee):
     paidapps_regestries = {"Metrics.Registries": PAIDAPPS_METRICS_REGISTRY}
 
     if datadog.is_enabled():
-        #paidapps_regestries["Metrics.Registries"].append(STATSD_REGESTRY)
+        paidapps_regestries["Metrics.Registries"].append(STATSD_REGESTRY)
 
-        logging.info(f"DUMMY LOG, {STATSD_REGESTRY}")
+        logging.info(f"DUMMY LOG, {paidapps_regestriess}")
 
     return paidapps_regestries
 
